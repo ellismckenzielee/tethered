a1be564cb0255011cc71dfc163d74085e99cd4b0
Ellis Lee <ellismckenzielee@gmail.com>
2021-12-07 17:08:40 +0000

    add the CreateUser component

---
+++ src/components/CreateUser.jsx
@@ -0,0 +1,29 @@
+import React, { useContext, useState } from "react";
+import { TouchableOpacity, Image, TextInput, Text, View } from "react-native";
+import styles from "../styles/Login.Style";
+import { UserContext } from "../contexts/UserContext";
+
+export default function CreateUser() {
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <Image></Image>
+      <Text>Enter Details</Text>
+      <Text>Email</Text>
+      <TextInput placeholder="example@google.com" onChangeText={setEmail} />
+      <Text>Password</Text>
+      <TextInput placeholder="password" onChangeText={setPassword} />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          setCurrentUser({ username: email, password: password });
+        }}
+      >
+        <Text>Sign Up!</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
