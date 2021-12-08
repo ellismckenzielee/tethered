1b1ba5d79875bc345d5ec71a3dcd781922c8bd7a
Tom Connor <tomconnor@Toms-MacBook-Air.local>
2021-12-08 16:25:32 +0000

    refactors Event Component to contain a separate Map Component

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,17 +1,8 @@
-import React, { useContext, useState, useEffect } from "react";
-import {
-  Button,
-  Image,
-  TextInput,
-  Text,
-  View,
-  TouchableOpacity,
-} from "react-native";
+import React, { useContext } from "react";
+import { View } from "react-native";
 import styles from "../styles/Login.Style";
+import Map from "./Map";
 import { UserContext } from "../contexts/UserContext";
-import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
-import getDeltas from "../utils/utils.maps";
-import * as Location from "expo-location";
 
 export default function Event({ navigation }) {
   const { isLoggedIn, currentUser } = useContext(UserContext);
@@ -25,79 +16,10 @@ export default function Event({ navigation }) {
     { name: "Sam", latitude: 53.47744, longitude: -2.28777 },
     { name: "Tom", latitude: 53.430156, longitude: -2.365864 },
   ];
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-
-  let maxLatitudeDelta;
-  let maxLongitudeDelta;
-
-  if (location !== null) {
-    const deltas = getDeltas(location, locations);
-    maxLatitudeDelta = deltas.maxLatitudeDelta;
-    maxLongitudeDelta = deltas.maxLongitudeDelta;
-  }
-
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        setErrorMsg("Permission to access location was denied");
-        return;
-      }
-      Location.watchPositionAsync({ distanceInterval: 100 }, (location) => {
-        setLocation(location);
-      });
-    })();
-  }, []);
-
-  if (location === null)
-    return (
-      <View>
-        <Text>Map Loading...</Text>
-      </View>
-    );
 
   return (
     <View style={styles.container}>
-      <Text>{location.coords?.latitude}</Text>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        initialRegion={{
-          latitude: location.coords?.latitude,
-          longitude: location.coords?.longitude,
-          latitudeDelta: maxLatitudeDelta * 2,
-          longitudeDelta: maxLongitudeDelta * 2,
-        }}
-      >
-        <Marker
-          coordinate={{
-            latitude: location.coords?.latitude,
-            longitude: location.coords?.longitude,
-          }}
-        >
-          <Image
-            source={require("../assets/userMarker.png")}
-            style={styles.marker}
-          ></Image>
-        </Marker>
-        {locations.map((location) => {
-          return (
-            <Marker
-              key={location.name}
-              coordinate={{
-                latitude: location.latitude,
-                longitude: location.longitude,
-              }}
-            >
-              <Image
-                source={require("../assets/groupMarker.png")}
-                style={styles.marker}
-              ></Image>
-            </Marker>
-          );
-        })}
-      </MapView>
+      <Map user={user} locations={locations} />
     </View>
   );
 }
