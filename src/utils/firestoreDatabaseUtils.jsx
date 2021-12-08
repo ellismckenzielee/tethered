6f4a07a97f23058ca1d3014276432e9ba79af2df
Sam <89224927+CoderJenks@users.noreply.github.com>
2021-12-08 16:55:45 +0000

    adds createANewEvent function to create an event subcollection for a group in Firestore

--- src/utils/firestoreDatabaseUtils.jsx
+++ src/utils/firestoreDatabaseUtils.jsx
@@ -1,15 +1,23 @@
 
-import {addDoc, collection} from 'firebase/firestore';
+import {addDoc, collection, doc} from 'firebase/firestore';
 import {db} from '../../firebase-config'
 
 // firestore - create new group
-const groupsCollection = collection(db, 'groups');
-
 async function createANewGroup(currentUser) {
-  const newGroup = await addDoc(groupsCollection, {
+  const newGroup = await addDoc(collection(db, 'groups'), {
     groupAdmin: currentUser,
   });
   // console.log the document path in the firestore database within the "groups" collection 
   console.log(`A new group was created at ${newGroup.path}`);
 }
-export {createANewGroup};
+
+// firestore - create new event within group
+async function createANewEvent(currentUser, groupDocId) {
+  const newEvent = await addDoc(collection(db, `groups/${groupDocId}/events`), {
+    eventAdmin: currentUser,
+  });
+  // console.log the document path in the firestore database within the "events" collection 
+  console.log(`A new event was created at ${newEvent.path}`);
+}
+
+export {createANewGroup, createANewEvent};
