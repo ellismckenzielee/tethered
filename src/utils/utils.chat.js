0b872279bd1d72017d87716fb210a4aa08aec88e
Ellis Lee <ellismckenzielee@gmail.com>
2021-12-15 20:42:06 +0000

    remove console.logs from chat

--- src/utils/utils.chat.js
+++ src/utils/utils.chat.js
@@ -3,21 +3,17 @@ import { db } from "../../firebase-config";
 export const watchGroupChat = async (groupId, cb) => {
   const docRef = doc(db, `groupChats`, `${groupId}`);
   const docSnap = await getDoc(docRef);
-  console.log(groupId);
   if (!docSnap.exists()) {
-    console.log("creating doc");
     try {
       await setDoc(doc(db, "groupChats", groupId), { groupId: groupId });
     } catch (err) {
       console.log(err);
     }
-    console.log("created");
   }
   const q = query(collection(db, `groupChats/${groupId}/messages`), orderBy("created_at"));
   const unsubscribe = onSnapshot(q, (querySnapshot) => {
     cb(querySnapshot);
   });
-  console.log(true);
   return unsubscribe;
 };
 
@@ -28,5 +24,7 @@ export const sendMessage = async (groupId, message, author, setMessage) => {
     created_at: Timestamp.now(),
   };
   const returnedMessage = await addDoc(collection(db, "groupChats", `${groupId}`, "messages"), newMessage);
-  setMessage("");
+  if (setMessage) {
+    setMessage("");
+  }
 };
