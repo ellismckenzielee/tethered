d2939df9fbbeae6494d38528546bdb5ae27a8af4
Ellis Lee <ellismckenzielee@gmail.com>
2021-12-15 00:06:10 +0000

    add logout component for navbar

---
+++ src/components/Logout.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { View, Button } from "react-native";
+import styles from "../styles/Login.Style";
+import { UserContext } from "../contexts/UserContext";
+import { useNavigation } from "@react-navigation/native";
+
+export default function Logout() {
+  const { setCurrentUser, isLoggedIn } = useContext(UserContext);
+  const navigation = useNavigation();
+
+  return (
+    <Button
+      onPress={() => {
+        setCurrentUser({});
+        navigation.navigate("Home");
+      }}
+      title={"Logout"}
+    ></Button>
+  );
+}
